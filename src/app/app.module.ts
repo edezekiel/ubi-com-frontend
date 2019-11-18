164154bfe5f74e0b0fea89d5d21f12935e827c8b
Ed Ezekiel <ed@ubiquisoft.com>
2019-11-18 15:58:36 -0600

    Add firebase hosting and deployment to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZGS3hBxxPloWgr8UGxFVWw8Z7h7YRIqg",
+  authDomain: "ubi-com-frontend.firebaseapp.com",
+  databaseURL: "https://ubi-com-frontend.firebaseio.com",
+  projectId: "ubi-com-frontend",
+  storageBucket: "ubi-com-frontend.appspot.com",
+  messagingSenderId: "501291955869",
+  appId: "1:501291955869:web:b7c2ed7ff998a228918674",
+  measurementId: "G-LWG97GTL4C"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +22,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
